281764af21b63e69e0d4afedd09bfbb8ca4c6497
JaniceTang713 <101858826+JaniceTang713@users.noreply.github.com>
2022-05-02 14:18:26 +0800

    This is another challenge file

---
+++ testgit.py
@@ -0,0 +1,21 @@
+# Python program to explain os.system() method
+	
+# importing os module
+import os
+
+# Command to execute
+# Using Windows OS command
+#cmd = "mkdir testingfile"
+#cmd1 = "cd challenge1"
+cmd2 = "git init"
+cmd3 = "touch add_file"
+cmd4 = "git add ."
+cmd5 = "git commit -m 'This is to add another file for testing' "
+
+# Using os.system() method
+#os.system(cmd)
+#os.get.cwd(challenge1)
+os.system(cmd2)
+os.system(cmd3)
+os.system(cmd4)
+os.system(cmd5)
